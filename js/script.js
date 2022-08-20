4ce40913076f1a39de1a3b22ebe0e30db5cb6c94
NeckBlick <nickmestre200@gmail.com>
2022-08-20 17:06:05 -0300

    upgrade conversor

--- js/script.js
+++ js/script.js
@@ -5,9 +5,13 @@ const button = document.querySelector('#button')
 
 
 function Converter(){
-    var result = Number(brcoin.value * Vcoin)
-    var resultFormatado = result.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-    lcoin.value = resultFormatado
+    if(brcoin.value == "" && lcoin.value != ""){
+        brcoin.value = lcoin.value / Vcoin
+    }else{
+        var result = Number(brcoin.value * Vcoin)
+        var resultFormatado = result.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+        lcoin.value = resultFormatado
+    }
 }
 
 
